2a5ae536d110cdb1f229a62260d2bed49c1663fc
Simon Rice <im@simonrice.com>
2017-05-21 13:59:31 +1000

    Carthage experimentation

--- Podfile
+++ Podfile
@@ -3,7 +3,7 @@ platform :ios, '8.0'
 
 use_frameworks!
 
-target 'RxBrightFutures' do
+target 'RxBrightFuturesExample' do
   pod 'BrightFutures', "~> 5.1"
   pod 'RxSwift', "~> 3.0"
 end
---
+++ RxBrightFutures/RxBrightFutures.h
@@ -0,0 +1,19 @@
+//
+//  RxBrightFutures.h
+//  RxBrightFutures
+//
+//  Created by Simon Rice on 20/05/2017.
+//  Copyright © 2017 Bonto.ch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RxBrightFutures.
+FOUNDATION_EXPORT double RxBrightFuturesVersionNumber;
+
+//! Project version string for RxBrightFutures.
+FOUNDATION_EXPORT const unsigned char RxBrightFuturesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxBrightFutures/PublicHeader.h>
+
+
